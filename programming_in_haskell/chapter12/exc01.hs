56f9c6be542bf8d9a0a380c190e322534cec22fe
Cassin01 <koyojb@gmail.com>
2018-11-23 17:25:47 +0900

    add ch12

--- programming_in_haskell/chapter12/exc01.hs
+++ programming_in_haskell/chapter12/exc01.hs
@@ -18,5 +18,31 @@ fst (1 + 2, 2 + 3)
   (\x -> 1 + x) -> outermost
 --}
 
-
+-- 2
 -- 評価を停止できること
+-- また、
+-- ２引数目が与えなれなくても実行ができること
+--
+-- Enable to stop evaluation,
+-- or
+-- Enable to  execute without second argument.
+
+-- 3
+-- mult = \x -> (\y -> x ∗ y)
+  -- 0 mult 3 4
+  -- 1 \x -> (\y -> x * y) 3 4
+  -- 2 \x -> (x * 4)
+  -- 3 (3 * 4)
+  -- 4 12
+
+-- 4
+fibs :: [Integer]
+fibs = 0 : 1 : [x + y | (x, y) <- zip fibs (tail fibs)]
+
+-- [0:1] [1]
+-- [0:1:2] [1:2]
+-- [0:1:2:3] [1:2:3]
+
+-- 5
+fib :: Int -> Integer
+fib n = fibs !! n
